706ffc65acecfcdf716c44ffe4ab2566c296bfee
shagbag913 <sh4gbag913@gmail.com>
2019-03-02 15:31:15 -0500

    vendor: set our own date prop
    This may seem stupid, however. ATM, our ro.build.date.utc does not match
    up with the zip file name. This is because these get set at different
    times.
    
    Signed-off-by: shagbag913 <sh4gbag913@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -6,6 +6,7 @@ PRODUCT_VERSION_MAJOR = Pie
 PRODUCT_VERSION_MINOR = niceparse.Goodbye_Star_Girl
 BOOTLEGGERS_VERSION_NUMBER := 4.0-Stable
 BOOTLEGGERS_SONGCODEURL = http://bit.ly/2Aa2WmA
+BOOTLEGGERS_EPOCH := $(shell date +%s)
 BOOTLEGGERS_POSTFIX := -$(shell date +"%Y%m%d-%H%M%S")
 
 ifndef BOOTLEGGERS_BUILD_TYPE
